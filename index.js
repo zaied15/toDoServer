ff5b2f1870fd34098dc6ced0275ab59844a0f264
unknown <mda800@gmail.com>
2022-05-19 02:53:35 +0600

    post method done

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://dbToDo:<password>@cluster0.rvb38.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvb38.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 async function run() {
   try {
+    await client.connect();
+    const postCollection = client.db("toDo").collection("post");
+
+    app.post("/post", async (req, res) => {
+      const obj = req.body;
+      const result = await postCollection.insertOne(obj);
+      res.send(result);
+    });
   } finally {
   }
 }
