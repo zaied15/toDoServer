677bfb48ee3d530b72518637c1140c0b67a2401d
unknown <mda800@gmail.com>
2022-05-19 03:41:30 +0600

    get and delete done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvb38.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -24,6 +24,18 @@ async function run() {
     app.post("/post", async (req, res) => {
       const obj = req.body;
       const result = await postCollection.insertOne(obj);
+      res.send(result);
+    });
+
+    app.get("/posts", async (req, res) => {
+      const result = await postCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await postCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
